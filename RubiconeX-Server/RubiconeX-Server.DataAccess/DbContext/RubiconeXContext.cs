2b296bc07c80ceae66f3477e25680acd8ea9a445
Zhuludkovr <82827317+NEXT-GM@users.noreply.github.com>
2021-11-20 17:52:21 +0300

    trt

---
+++ RubiconeX-Server/RubiconeX-Server.DataAccess/DbContext/RubiconeXContext.cs
@@ -0,0 +1,20 @@
+﻿using RubiconeX_Server.DataAccess.Core.Interfaces.Context;
+using Microsoft.EntityFrameworkCore;
+using RubiconeX_Server.DataAccess.Core.Models;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace RubiconeX_Server.DataAccess.DbContext
+{
+    public class RubiconeXContext : Microsoft.EntityFrameworkCore.DbContext, IRubicContext
+    {
+        public RubiconeXContext(DbContextOptions<RubiconeXContext> options): base(options)
+        { 
+        }
+
+        public DbSet<UserRto> Users { get; set; }
+        public DbSet<UserRoleRto> UserRoles { get; set; }
+
+
+    }
+}
